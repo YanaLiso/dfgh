e5bffa0ee0e07a9cff02b58b8e57796e8be9a3fd
YanaLiso <114093396+YanaLiso@users.noreply.github.com>
2022-10-05 18:13:16 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//задача 1
+
+	/*int hp;
+	cin >> hp;
+	if (hp <= 0) {
+		cout << "false" << endl;
+	}
+	else {
+		cout << "true" << endl;
+	}*/
+
+	//задача 2
+
+	/*int tmp;
+	cin >> tmp;
+	if (tmp > 20) {
+		cout << "on" << endl;
+	}
+	else {
+		cout << "off" << endl;
+	}*/
+
+	//задача 3
+
+	/*int vlag;
+	int T;
+	int tec ;
+	cin >> vlag >> T >> tec;
+	if (vlag > 80) {
+		cout << "off" << endl;
+	}
+	else if (T < tec && vlag <= 80) {
+		cout << "on" << endl;
+		}*/
+
+	//задача 4
+
+	/*double x;
+	cin >> x;
+	if (x > 0) {
+		cout << sqrt(x - 2) << endl;
+	}
+	else if (x = 0) {
+		cout << 0 << endl;
+	}
+	else if (x < 0) {
+		cout << abs(x) << endl;
+	}*/
+
+	//задача 5
+
+	/*int num;
+	cin >> num;
+	switch (num) {
+	case 1:
+	case 10: cout << "thumb" << endl; break;
+	case 2:
+	case 9: cout << "forefinger" << endl; break;
+	case 3:
+	case 8: cout << "middle finger" << endl; break;
+	case 4:
+	case 7: cout << "ring finger" << endl; break;
+	case 5: 
+	case 6: cout << "little finger" << endl; break;*/
+
+	//задача 6
+
+	int temk;
+	int okn;
+	cin >> temk >> okn;
+
+	if (okn < temk) {
+		cout << "open window" << endl;
+	}
+	else if (temk > 20) {
+		cout << "ac on" << endl;
+	}
+	else  {
+		cout << "ac off" << endl;
+	}
+	
+
+}
